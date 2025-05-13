25bfbef5bccb044e25102a23059d29ebc82d9055
Rominavila47 <rominavila656@gmail.com>
2025-05-13 18:09:59 -0300

    initial

---
+++ src/main/java/buhorisLibrary/BuhorisApplication.java
@@ -0,0 +1,13 @@
+package buhorisLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuhorisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuhorisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/buhorisLibrary/BuhorisApplicationTests.java
@@ -0,0 +1,13 @@
+package buhorisLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuhorisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
